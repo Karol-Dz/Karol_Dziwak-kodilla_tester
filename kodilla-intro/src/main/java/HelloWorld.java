b32dd69240899279ab05a053b21bf18d39f4ce6c
Karol Dziwak <karol.dziwak@gmail.com>
2025-09-10 23:52:48 +0200

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld  {
+public static void main(String[] args) {         // [2]
+    System.out.println("Hello World!"); // [3]
+    System.out.println("Karol"); // [4]
+    System.out.println(42); // [5]
+    System.out.println('D'); // [6]
+   }
+}
