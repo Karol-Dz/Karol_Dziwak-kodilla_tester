e345d9ce1b76af2fa012585a3b4c0e052ef240fa
Karol Dziwak <karol.dziwak@gmail.com>
2025-09-12 22:00:18 +0200

    newClass

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Karol";
+        String mySurname = "Dziwak";
+        int myAge = 42;
+        double height = 1.83;
+        char carClass = 'B';
+        boolean isGoodTester = false;
+        System.out.println(myName);
+        System.out.println(mySurname);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodTester);
+    }
+}
